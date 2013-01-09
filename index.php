ed596d972e597d92cd9e710e747400fab9286f86
RCreyghton <r.creyghton@gmail.com>
2013-01-09 15:17:17 +0100

    index, header en register nu in een stijl

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?xml version="1.0"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Hello World!</title>
+	<title>Header-test</title>
 	<meta http-equiv="Contennt-Type" content="text/html; charset=iso-8859-1"/>
 </head>
 
 <body>
-	<h1>Hello World!</h1>
+    <div>
+    <h1>Hello World!</h1>
 	<p>Ramon's first HTML written in vi on webdb.science.uva.nl via SSH<p>
+    </ div>
 </body>
 
 </html>
