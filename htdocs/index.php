426b5fd0582ab69c626e8e03bc2c0a2a4a879f9e
RCreyghton <r.creyghton@gmail.com>
2013-01-16 16:31:35 +0100

    index.php voorzien van dynamische base-url mbv PHP. Onze class-probeersels qua ouput in een element-divje geplaatst.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>WebDBOverflow - Index</title>
+    <base href="<?php echo substr($_SERVER["PHP_SELF"],0, strlen($_SERVER["PHP_SELF"]) - 9)?>" target="_blank" /> 
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="header_wrapper wrapped">
+        <div class="header_title body_centered">
+          <h1>WebDBOverflow</h1>
+          <div class="header_service">
+            <ul>
+              <li>
+                <a href="login.php">Login</a>
+              </li>
+              <li>
+                <a href="register.php">Registreer</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+          <div class="content_open body_centered">
+            <ul class="menu">
+              <li><a href="index.php?forum=1" class="menulink">Forum 1</a></li>
+              <li><a href="index.php?forum=2" class="menulink">Forum 2</a></li>
+              <li><a href="index.php?forum=3" class="menulink">Forum 3</a></li>
+            </ul>
+          </div>
+        </div>
+
+      <div class="content_wrapper wrapped light_grey_gradient"> 
+        <div class="content body_centered">
+<?php
+    print "          <div class=\"element\">\n            <p>Get-variabele gevonden: q=". $_GET['q'] . "</p>\n          </div>\n";
+?>
+          <div class="element">
+            <p>Ruimte voor PHP-testoutput:</p>
+            <p>
 <?php
 
 
@@ -74,53 +116,12 @@ $c->save();
 
 
 
-
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>WebDBOverflow - Index</title>
-    <base href="/webdb13AD3/" target="_blank"> 
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
-  </head>
-  <body>
-    <div class="wrapper">
-      <div class="header_wrapper wrapped">
-        <div class="header_title body_centered">
-          <h1>WebDBOverflow</h1>
-          <div class="header_service">
-            <ul>
-              <li>
-                <a href="login.php">Login</a>
-              </li>
-              <li>
-                <a href="register.php">Registreer</a>
-              </li>
-            </ul>
+            </p>
           </div>
-          </div>
-          <div class="content_open body_centered">
-            <ul class="menu">
-              <li><a href="index.php?forum=1" class="menulink">Forum 1</a></li>
-              <li><a href="index.php?forum=2" class="menulink">Forum 2</a></li>
-              <li><a href="index.php?forum=3" class="menulink">Forum 3</a></li>
-            </ul>
-          </div>
-        </div>
-
-      <div class="content_wrapper wrapped light_grey_gradient"> 
-        <div class="content body_centered">
-<?php
-    print "          <div class=\"element\">\n            <p>Get-variabele gevonden: q=". $_GET['q'] . "</p>\n          </div>\n";
-?>
           <div class="element">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id sagittis nisl. Nulla elementum ligula ac orci commodo sed congue ipsum dignissim. Donec lacinia, eros a posuere mollis, neque nulla iaculis odio, vitae vulputate diam leo eget neque. Vivamus interdum arcu at velit euismod sit amet viverra augue rhoncus. Mauris vel enim eget lorem ultrices vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ut orci vulputate nulla malesuada fermentum sed eget lectus. Vivamus ut odio eu enim fermentum congue.</p>
             <p>Fusce enim nunc, laoreet a euismod eget, consectetur ac lorem. Maecenas eu augue et mauris porta vestibulum et fermentum quam. Nulla eget lorem purus. Cras at auctor elit. Duis sit amet eros enim. Integer gravida, sem non tempor hendrerit, eros arcu tincidunt sapien, in elementum tortor nibh et massa. Pellentesque vitae eros turpis, at blandit quam. Duis consectetur vulputate arcu eu convallis. Aliquam facilisis faucibus dolor, non aliquam libero ultricies a. Sed lacus risus, tempor quis tempus vitae, egestas at justo. Mauris consectetur leo quis dolor tristique id dapibus dolor tincidunt. Sed lacinia felis et ipsum imperdiet id congue lacus hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam erat volutpat. Nulla eget quam a elit suscipit volutpat. Praesent et ante et sapien tempor sollicitudin eu sed quam.</p>
-          </div>
-          <div class="element">
-            <p>Donec vel rhoncus nunc. Maecenas tristique consectetur ullamcorper. Vivamus volutpat, urna eget euismod dignissim, metus sem mattis nisl, nec tempor dui diam nec ligula. Curabitur scelerisque velit et turpis vulputate eget feugiat nisi mollis. Curabitur tristique convallis ipsum et consectetur. Etiam tempus, elit non eleifend sollicitudin, erat velit eleifend neque, quis luctus nisi sapien et leo. Fusce ligula metus, hendrerit eu elementum a, bibendum ac eros. Integer imperdiet, dui id hendrerit posuere, risus risus cursus massa, a porttitor dolor quam vel nunc. Sed varius sagittis suscipit. Mauris blandit facilisis mi, ut cursus leo dignissim ut. Ut adipiscing gravida orci. Sed ornare eleifend neque, sit amet mollis turpis dignissim sit amet. Curabitur ullamcorper convallis nisl, in commodo ante mattis quis. Vivamus non orci turpis, sit amet sollicitudin quam. </p>
           </div>
         </div>
       </div>
