2899dda82bc89c65dca79b60494f0d97610efc86
Frank van Luijn <frank@accode.nl>
2013-01-21 15:49:01 +0100

    Added template with placeholders

---
+++ htdocs/assets/template.php
@@ -0,0 +1,77 @@
+<?php
+if (!defined("WEBDB_EXEC"))
+	die("No direct access!");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>WebDBOverflow - <!-- TITLE --></title>
+		<base href="<?php echo substr($_SERVER["PHP_SELF"], 0, strlen($_SERVER["PHP_SELF"]) - 9) ?>" target="_blank" /> 
+		<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+  </head>
+  <body>
+		<div class="wrapper">
+			<div class="header_wrapper wrapped">
+				<div class="header_title body_centered">
+					<h1>WebDBOverflow</h1>
+					<div class="header_service">
+						<!-- LOGIN -->
+						<ul>
+							<li>
+								<a href="login.php">Login</a>
+							</li>
+							<li>
+								<a href="register.php">Registreer</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="content_open body_centered">
+					<!-- MENU -->
+					<ul class="menu">
+						<li><a href="index.php?forum=1" class="menulink">Forum 1</a></li>
+						<li><a href="index.php?forum=2" class="menulink">Forum 2</a></li>
+						<li><a href="index.php?forum=3" class="menulink">Forum 3</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="content_wrapper wrapped light_grey_gradient"> 
+				<div class="content body_centered">
+					<!-- CONTENT -->
+				</div>
+			</div>
+
+			<div class="footer_wrapper wrapped light_grey_gradient">
+				<div class="content_close body_centered">
+					<span>
+						&copy; WebDBOverflow - all rights reserved.
+					</span>
+				</div>
+
+				<div class="statistics body_centered">
+					<div class="statistics_container">
+						<h3><span class="hero_number">325</span> vragen</h3>
+						<h3><span class="hero_number">868</span> beantwoord</h3>
+						<h3><span class="hero_number">754</span> onbeantwoord</h3>
+						<h3><span class="hero_number">86.9%</span> verhouding</h3>
+					</div>
+
+					<div class="statistics_container">
+						<h3><span class="hero_number">325</span> categorie&euml;n</h3>
+						<h3><span class="hero_number">325</span> gebruikers</h3>
+						<h3><span class="hero_number">868</span> reacties</h3>
+					</div>
+
+					<div class="statistics_container">
+						<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11-blue" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+						<a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" /></a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+  </body>
+</html>
