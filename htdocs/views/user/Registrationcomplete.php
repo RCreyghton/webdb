41eb49888c5992eee4fd3e46ad35152494c307f3
RCreyghton <r.creyghton@gmail.com>
2013-01-22 22:36:32 +0100

    Added basic Views_Registrationcomplete class.

---
+++ htdocs/views/user/Registrationcomplete.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined("WEBDB_EXEC"))
+	die("No direct access!");
+
+class Views_User_Registrationform extends Views_Base {
+	
+	public $formresult = array();
+	
+	public function render() {
+		$this->title = "Geregistreerd";
+		echo "<h2>U bent succesvol geregistreerd</h2>\n<p>U kunt nu <a href=\"users/login/\">inloggen</a> met uw e-mailadres en wachtwoord.</p>";
+	}
+
+}
