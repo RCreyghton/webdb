aac611292034277ec9f3a9be12f7ecb05e829658
Shafiqa <s.ah@live.nl>
2013-02-02 02:22:51 +0100

    Update htdocs/assets/user_online.php

--- htdocs/assets/user_online.php
+++ htdocs/assets/user_online.php
@@ -12,6 +12,8 @@ $result3=mysql_query($sql3);
 
 
 $sql3="SELECT * FROM $tbl_name";
+$count_user_online=mysql_num_rows($result3);
+echo "User online : $count_user_online ";
 
 $result4=mysql_query($sql4);
 
