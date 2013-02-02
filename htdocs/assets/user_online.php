34566982640527135c1121b8a114e52e9b61f641
Shafiqa <s.ah@live.nl>
2013-02-02 02:32:47 +0100

    Update htdocs/assets/user_online.php

--- htdocs/assets/user_online.php
+++ htdocs/assets/user_online.php
@@ -1,6 +1,5 @@
 
 
-
 session_start();
 $session=session_id();
 $time=time();
@@ -18,26 +17,3 @@ echo "User online : $count_user_online ";
 
 $result4=mysql_query($sql4);
 
-// if over 10 minute, delete session 
-$sql4="DELETE FROM $tbl_name WHERE time<$time_check";
-echo "User online : $count_user_online ";
-// Open multiple browser page for result
-
-$count=mysql_num_rows($result);
-
-<?php
-
-
-
-if($count=="0"){
-
-$result1=mysql_query($sql1);
-}
-
-else {
-}
-
-
-// Close connection
-mysql_close();
-?>
