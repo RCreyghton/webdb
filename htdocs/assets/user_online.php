113c95839f7f43de4ddf43495e048dba3bfd4f31
Shafiqa <s.ah@live.nl>
2013-02-02 02:46:06 +0100

    Update htdocs/assets/user_online.php

---
+++ htdocs/assets/user_online.php
@@ -0,0 +1,9 @@
+<table width="400" border="0" align="center" cellpadding="3" cellspacing="1">
+<tr>
+<td><strong>Contact Form </strong></td>
+</tr>
+</table>
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1">
+<tr>
+<td><form name="form1" method="post" action="send_contact.php">
+<table width="100%" border="0" cellspacing="1" cellpadding="3">
