2b4d7e57da2a0116bfbd00f64e11a5cc2553c53b
Shafiqa <s.ah@live.nl>
2013-02-02 02:43:19 +0100

    Update htdocs/assets/user_online.php

--- htdocs/assets/user_online.php
+++ htdocs/assets/user_online.php
@@ -14,5 +14,9 @@
 <td>&nbsp;</td>
 <td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
 </tr>
-</table>
-</form>
+
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1">
+<tr>
+<td><form name="form1" method="post" action="send_contact.php">
+<table width="100%" border="0" cellspacing="1" cellpadding="3">
+
