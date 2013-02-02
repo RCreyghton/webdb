38806c4b01f7b7c48786f8dcf3476f62d1105c2a
Shafiqa <s.ah@live.nl>
2013-02-02 02:37:46 +0100

    Update htdocs/assets/user_online.php

--- htdocs/assets/user_online.php
+++ htdocs/assets/user_online.php
@@ -21,3 +21,22 @@
 </td>
 </tr>
 </table>
+
+<td width="82%"><input name="subject" type="text" id="subject" size="50"></td>
+</tr>
+<tr>
+<td>Detail</td>
+<td>:</td>
+<td><textarea name="detail" cols="50" rows="4" id="detail"></textarea></td>
+</tr>
+<tr>
+
+<td>Name</td>
+<td>:</td>
+<td><input name="name" type="text" id="name" size="50"></td>
+</tr>
+<tr>
+<td>Email</td>
+<td>:</td>
+<td><input name="customer_mail" type="text" id="customer_mail" size="50"></td>
+</tr>
