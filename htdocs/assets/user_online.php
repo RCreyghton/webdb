fd440d6edd0dd354d6db6f91389934dd74c01632
Shafiqa <s.ah@live.nl>
2013-02-02 02:42:33 +0100

    Update htdocs/assets/user_online.php

--- htdocs/assets/user_online.php
+++ htdocs/assets/user_online.php
@@ -1,1 +1,16 @@
+<table width="400" border="0" align="center" cellpadding="3" cellspacing="1">
+<tr>
+<td><strong>Contact Form </strong></td>
+</tr>
 
+<tr>
+<td width="16%">Subject</td>
+<td width="2%">:</td>
+
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
+</tr>
+</table>
+</form>
